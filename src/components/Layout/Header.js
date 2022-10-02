2a118e64c9107b738fde3ef69e90a9e1aec0ff0b
tqbrandao <tqbrandao@gmail.com>
2022-10-02 09:49:15 -0300

    Updated color pallete

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>
+        <h1>React Food Order App</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
